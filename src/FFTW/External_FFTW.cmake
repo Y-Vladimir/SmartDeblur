3b088bd0e61b54cbca2ad9fc1ac463b63b68593d
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2012-11-06 17:36:19 -0500

    ENH: Added better support for system FFTW

--- src/FFTW/External_FFTW.cmake
+++ src/FFTW/External_FFTW.cmake
@@ -27,10 +27,8 @@ set(FFTW_THREADS_CONFIGURATION --enable-threads)
 ## Perhaps in the future a set of TryCompiles could be used here.
 set(FFTW_OPTIMIZATION_CONFIGURATION "" CACHE INTERNAL "architecture flags: --enable-sse --enable-sse2 --enable-altivec --enable-mips-ps --enable-cell")
 if(USE_SYSTEM_FFTW)
-  find_package( FFTW )
-  link_directories(${FFTW_LIBDIR})
-  set(FFTW_LIBRARIES ${FFTW_LIBDIR}${CMAKE_STATIC_LIBRARY_PREFIX}fftw3${CMAKE_STATIC_LIBRARY_SUFFIX}
-	${FFTW_LIBDIR}${CMAKE_STATIC_LIBRARY_PREFIX}fftw3_threads${CMAKE_STATIC_LIBRARY_SUFFIX})
+  find_library(FFTW_LIBRARIES ${CMAKE_SHARED_LIBRARY_PREFIX}fftw3${CMAKE_SHARED_LIBRARY_SUFFIX})
+  find_path(FFTW_INCLUDES fftw3.h)
 else()
   set(FFTW_COMPILER_FLAGS
     CC=${CMAKE_C_COMPILER}
@@ -41,8 +39,8 @@ else()
   if(WIN32 AND NOT MINGW)
     message("Can't build fftw as external project on Windows")
     message("Using pre-compiled fftw library")
-	set(FFTW_LIBRARIES ${CMAKE_CURRENT_SOURCE_DIR}/FFTW/libs/libfftw3-3.lib)
-	set(FFTW_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/FFTW)
+	  set(FFTW_LIBRARIES ${CMAKE_CURRENT_SOURCE_DIR}/FFTW/libs/libfftw3-3.lib)
+	  set(FFTW_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/FFTW)
   else()
     #
     # fftw limitation -- can't be built in
@@ -51,36 +49,33 @@ else()
       message(FATAL_ERROR
         "Can't build fftw in a directory with whitespace in its name")
     endif()
-    #
-    # build fftw as an external project
-    if(BUILD_SHARED_LIBS)
-      set(FFTW_SHARED_FLAG --enable-shared)
-    endif()
-	set(FFTW_VERSION 3.3.2)
-	set(FFTW_MD5 "6977ee770ed68c85698c7168ffa6e178")
-	set(FFTW_URL_PRE "ftp://ftp.fftw.org/pub/fftw/fftw-")
-	set(FFTW_URL_POST ".tar.gz")
-	set(FFTW_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/fftw/install)
-	set(FFTW_LIBRARIES_PATH ${FFTW_INSTALL_DIR}/lib/)
-	set(FFTW_LIBRARIES
-	${FFTW_LIBRARIES_PATH}${CMAKE_STATIC_LIBRARY_PREFIX}fftw3${CMAKE_STATIC_LIBRARY_SUFFIX}
-	${FFTW_LIBRARIES_PATH}${CMAKE_STATIC_LIBRARY_PREFIX}fftw3_threads${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(FFTW_SHARED_FLAG --enable-shared)
+  	set(FFTW_VERSION 3.3.2)
+  	set(FFTW_MD5 "6977ee770ed68c85698c7168ffa6e178")
+  	set(FFTW_URL_PRE "ftp://ftp.fftw.org/pub/fftw/fftw-")
+  	set(FFTW_URL_POST ".tar.gz")
+  	set(FFTW_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/fftw/install)
+  	set(FFTW_LIBRARIES_PATH ${FFTW_INSTALL_DIR}/lib/)
+  	set(FFTW_LIBRARIES
+      ${FFTW_LIBRARIES_PATH}${CMAKE_SHARED_LIBRARY_PREFIX}fftw3${CMAKE_SHARED_LIBRARY_SUFFIX}
+      ${FFTW_LIBRARIES_PATH}${CMAKE_SHARED_LIBRARY_PREFIX}fftw3_threads${CMAKE_SHARED_LIBRARY_SUFFIX})
 
-	ExternalProject_add(FFTW
-	  URL ${FFTW_URL_PRE}${FFTW_VERSION}${FFTW_URL_POST}
-	  URL_MD5 ${FFTW_MD5}
-	  SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/fftw/src
-      BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/fftw/src
-      INSTALL_DIR ${FFTW_INSTALL_DIR}
-	  UPDATE_COMMAND ""
-	  CONFIGURE_COMMAND <SOURCE_DIR>/configure
-	    ${FFTW_SHARED_FLAG}
-	    ${FFTW_OPTIMIZATION_CONFIGURATION}
-	    ${FFTW_THREADS_CONFIGURATION}
-	    --disable-fortran
-	    --prefix=<INSTALL_DIR>
-	    ${FFTW_COMPILER_FLAGS}
-	)
+    include(ExternalProject)
+  	ExternalProject_add(FFTW
+  	  URL ${FFTW_URL_PRE}${FFTW_VERSION}${FFTW_URL_POST}
+  	  URL_MD5 ${FFTW_MD5}
+  	  SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/fftw/src
+        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/fftw/src
+        INSTALL_DIR ${FFTW_INSTALL_DIR}
+  	  UPDATE_COMMAND ""
+  	  CONFIGURE_COMMAND <SOURCE_DIR>/configure
+  	    ${FFTW_SHARED_FLAG}
+  	    ${FFTW_OPTIMIZATION_CONFIGURATION}
+  	    ${FFTW_THREADS_CONFIGURATION}
+  	    --disable-fortran
+  	    --prefix=<INSTALL_DIR>
+  	    ${FFTW_COMPILER_FLAGS}
+  	)
 
     set(FFTW_INCLUDES ${FFTW_INSTALL_DIR}/include)
   endif()
